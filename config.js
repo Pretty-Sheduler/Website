ee0a5781781e8e4a40897aea988a18175b782c33
NickWeaver02 <anweaver02@gmail.com>
2025-07-26 20:46:56 -0700

    j

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ window.APP_CONFIG = {
   CLIENT_ID: '6887lmtltpfidijg07a34bpps4',
   AUTHORITY: 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_HhsI9dSLk',
   SCOPE: 'openid email profile',
-  POST_LOGOUT_URI: "https://pretty-scheduler.github.io/Website/logout.html",
+  POST_LOGOUT_URI: "https://pretty-scheduler.github.io/Website/index.html",
   AWS_DOMAIN: 'https://us-east-2HhsI9dSLk.auth.us-east-2.amazoncognito.com'
 };
