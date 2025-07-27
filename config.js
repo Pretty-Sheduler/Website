4d169847b0be26db6710cf9f8bcaa4338249018c
NickWeaver02 <anweaver02@gmail.com>
2025-07-26 21:27:31 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 window.APP_CONFIG = {
-  REDIRECT_URI: 'https://pretty-scheduler.github.io/Website/',
+  REDIRECT_URI: 'https://pretty-sheduler.github.io/Website/',
   CLIENT_ID: '6887lmtltpfidijg07a34bpps4',
   AUTHORITY: 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_HhsI9dSLk',
   SCOPE: 'openid email profile',
-  POST_LOGOUT_URI: "https://pretty-scheduler.github.io/Website/",
+  POST_LOGOUT_URI: "https://pretty-sheduler.github.io/Website/",
   AWS_DOMAIN: 'https://us-east-2HhsI9dSLk.auth.us-east-2.amazoncognito.com'
 };
