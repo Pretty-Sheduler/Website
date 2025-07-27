580c95d3daa5c070409c22d142c62c4d5f8b240d
NickWeaver02 <anweaver02@gmail.com>
2025-07-26 21:24:24 -0700

    IM AN IDOT

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 window.APP_CONFIG = {
-  REDIRECT_URI: 'https://pretty-sheduler.github.io/Website/',
+  REDIRECT_URI: 'https://pretty-scheduler.github.io/Website/',
   CLIENT_ID: '6887lmtltpfidijg07a34bpps4',
   AUTHORITY: 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_HhsI9dSLk',
   SCOPE: 'openid email profile',
