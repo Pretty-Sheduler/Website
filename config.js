f51bdcda9eee20547fb01491cb7607a6fb1cda5b
NickWeaver02 <anweaver02@gmail.com>
2025-07-26 19:51:45 -0700

    pain

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 window.APP_CONFIG = {
   REDIRECT_URI: 'https://pretty-sheduler.github.io/Website/',
-  CLIENT_ID: '6887lmtltpfidijg07a34bpps4',
-  AUTHORITY: 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_HhsI9dSLk',
+  CLIENT_ID: '1o5fle2v6u98mtsoivuus90382',
+  AUTHORITY: 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_NAxGjIiVI',
   SCOPE: 'openid email profile',
   POST_LOGOUT_URI: 'https://pretty-sheduler.github.io/Website/',
   AWS_DOMAIN: 'https://HhsI9dSLk.auth.us-east-2.amazoncognito.com'
